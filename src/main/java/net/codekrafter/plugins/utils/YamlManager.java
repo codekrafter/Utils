b3031f0d0b882724a097a27ce7ca7161f855e1dd
codekrafter <codekrafter@gmail.com>
2015-06-18 10:59:38 -0400

    Finilized V1.0

--- src/main/java/net/codekrafter/plugins/utils/YamlManager.java
+++ src/main/java/net/codekrafter/plugins/utils/YamlManager.java
@@ -1,4 +1,3 @@
-
 package net.codekrafter.plugins.utils;
 
 import java.io.File;
@@ -10,8 +9,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class YamlManager
-{
+public class YamlManager {
 
 	public static JavaPlugin plugin;
 	public File path;
@@ -19,74 +17,60 @@ public class YamlManager
 	private File customConfigFile = null;
 	private String name;
 
-	public YamlManager(JavaPlugin plugin, String name)
-	{
+	public YamlManager(JavaPlugin plugin, String name) {
 		YamlManager.plugin = plugin;
 		this.name = name;
 	}
-	
-	public YamlManager(JavaPlugin plugin, String name, String path, boolean relitiveToDataFolder)
-	{
+
+	public YamlManager(JavaPlugin plugin, String name, String path,
+			boolean relitiveToDataFolder) {
 		YamlManager.plugin = plugin;
 		this.name = name;
-		if(relitiveToDataFolder) {
+		if (relitiveToDataFolder) {
 			this.path = new File(plugin.getDataFolder(), path);
 		} else {
 			this.path = new File(path);
 		}
 	}
 
-	public void reloadCustomConfig()
-	{
-		if (customConfigFile == null)
-		{
+	public void reloadCustomConfig() {
+		if (customConfigFile == null) {
 			customConfigFile = new File(path, name + ".yml");
 		}
 
 		customConfig = YamlConfiguration.loadConfiguration(customConfigFile);
 		InputStream defConfigStream = plugin.getResource(name + ".yml");
-		if (defConfigStream != null)
-		{
+		if (defConfigStream != null) {
 			YamlConfiguration defConfig = YamlConfiguration
 					.loadConfiguration(defConfigStream);
 			customConfig.setDefaults(defConfig);
 		}
 	}
 
-	public FileConfiguration getCustomConfig()
-	{
-		if (customConfig == null)
-		{
+	public FileConfiguration getCustomConfig() {
+		if (customConfig == null) {
 			reloadCustomConfig();
 		}
 		return customConfig;
 	}
 
-	public void saveCustomConfig()
-	{
-		if (customConfig == null || customConfigFile == null)
-		{
+	public void saveCustomConfig() {
+		if (customConfig == null || customConfigFile == null) {
 			return;
 		}
-		try
-		{
+		try {
 			getCustomConfig().save(customConfigFile);
-		}
-		catch (IOException ex)
-		{
+		} catch (IOException ex) {
 			plugin.getLogger().log(Level.SEVERE,
 					"Could not save config to " + customConfigFile, ex);
 		}
 	}
 
-	public void saveDefaultConfig()
-	{
-		if (customConfigFile == null)
-		{
+	public void saveDefaultConfig() {
+		if (customConfigFile == null) {
 			customConfigFile = new File(path, name + ".yml");
 		}
-		if (!customConfigFile.exists())
-		{
+		if (!customConfigFile.exists()) {
 			plugin.saveResource(name + ".yml", false);
 		}
 	}
